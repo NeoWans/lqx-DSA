baecfead6eb06be0face08edad88b3bf5cfc28ae
ArcanusNEO <lucasding@foxmail.com>
2021-11-28 22:26:41 +0800

    .

--- work-5.1/src/gen-testbench.cc
+++ work-5.1/src/gen-testbench.cc
@@ -4,5 +4,9 @@ signed main() {
   ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
   random_device rd;
   mt19937 mt(rd());
-  cout << mt19937::min() << " " << mt19937::max() << endl;
+  uniform_int_distribution uidist(0, INT32_MAX);
+  const int n = 10000;
+  cout << n << endl;
+  for (int i = 0; i < n; ++i)
+    cout << uidist(mt) << " ";
 }

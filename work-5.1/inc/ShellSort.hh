aa558b20aa45252d2776a248f1d009ce8a865b27
ArcanusNEO <lucasding@foxmail.com>
2021-11-28 22:08:07 +0800

    .

---
+++ work-5.1/inc/ShellSort.hh
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+template<typename Iterator>
+void ShellSort(Iterator first, Iterator last) {
+  int len = last - first, step;
+  while (step < len / 3)
+    step = 3 * step + 1;
+  while (step >= 1) {
+    for (auto i = step; i < len; i++)
+      for (auto j = i; j >= step && comp(*(first + j), *(first + (j - step))); j -= step)
+        swap(*(first + j), *(first + (j - step)));
+    step = step / 3;
+  }
+}

791a206d651f810641af85f1559e7b3bee097f72
ArcanusNEO <lucasding@foxmail.com>
2021-11-28 22:13:11 +0800

    .

---
+++ work-5.1/src/gen-testbench.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+  random_device rd;
+  mt19937 mt(rd());
+  cout << mt19937::min() << " " << mt19937::max() << endl;
+}
